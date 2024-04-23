f36b1ac263bed11a500eeb8a9402a0137af1b4d3
Lee <lee.jensen@agilix.com>
2024-04-23 09:32:00 -0600

    add(starter-code) for all deliverables

---
+++ starter-code/3-web-api/dataaccess/DataAccessException.java
@@ -0,0 +1,10 @@
+package dataaccess;
+
+/**
+ * Indicates there was an error connecting to the database
+ */
+public class DataAccessException extends Exception{
+    public DataAccessException(String message) {
+        super(message);
+    }
+}
