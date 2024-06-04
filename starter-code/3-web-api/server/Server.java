94ae4c1d6bcbf467d7769650fb983b8a3c826171
Alex Burgess <49875526+Truthwatcher64@users.noreply.github.com>
2024-06-04 21:42:33 +0000

    Update Server.java
    Added the Spark.init to help with starting the server from the starter code

--- starter-code/3-web-api/server/Server.java
+++ starter-code/3-web-api/server/Server.java
@@ -11,6 +11,9 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
         Spark.awaitInitialization();
         return Spark.port();
     }
