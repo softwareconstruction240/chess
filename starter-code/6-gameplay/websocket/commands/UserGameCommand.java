4c18d5f697d0f7f93ce9cfbcac429092c5e63ec9
Michael Davenport <52420978+19mdavenport@users.noreply.github.com>
2024-08-08 13:59:36 -0600

    Fix UserGameCommand

--- starter-code/6-gameplay/websocket/commands/UserGameCommand.java
+++ starter-code/6-gameplay/websocket/commands/UserGameCommand.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 /**
  * Represents a command a user can send the server over a websocket
- * 
+ *
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */
@@ -51,12 +51,12 @@ public class UserGameCommand {
         }
         UserGameCommand that = (UserGameCommand) o;
         return getCommandType() == that.getCommandType() &&
-                Objects.equals(getAuthString(), that.getAuthString()) &&
+                Objects.equals(getAuthToken(), that.getAuthToken()) &&
                 Objects.equals(getGameID(), that.getGameID());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getCommandType(), getAuthString(), getGameID());
+        return Objects.hash(getCommandType(), getAuthToken(), getGameID());
     }
 }
