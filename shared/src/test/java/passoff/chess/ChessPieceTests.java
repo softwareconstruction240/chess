1928db07537b9df480271e7df94aa94443935759
James Finlinson <the30clues@gmail.com>
2025-01-28 08:59:21 -0700

    Add thoroughness to ChessPieceTests allDifferent

--- shared/src/test/java/passoff/chess/ChessPieceTests.java
+++ shared/src/test/java/passoff/chess/ChessPieceTests.java
@@ -20,7 +20,11 @@ public class ChessPieceTests extends EqualsTestingUtility<ChessPiece> {
     @Override
     protected Collection<ChessPiece> buildAllDifferent() {
         return List.of(
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN)
+                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING),
+                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN),
+                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN),
+                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN),
+                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)
         );
     }
 
