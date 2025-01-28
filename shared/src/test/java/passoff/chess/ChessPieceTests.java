d94babeb6368933e414fb5a5d9d0b031ae90b116
James Finlinson <the30clues@gmail.com>
2025-01-28 08:43:44 -0700

    Introduce EqualsTestingUtility and install in ChessPieceTests

--- shared/src/test/java/passoff/chess/ChessPieceTests.java
+++ shared/src/test/java/passoff/chess/ChessPieceTests.java
@@ -2,41 +2,31 @@ package passoff.chess;
 
 import chess.ChessGame;
 import chess.ChessPiece;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
-public class ChessPieceTests {
-    private ChessPiece original;
-    private ChessPiece equal;
-    private ChessPiece different;
-    @BeforeEach
-    public void setUp() {
-        original = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
-        equal = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
-        different = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+import java.util.Collection;
+import java.util.List;
+
+public class ChessPieceTests extends EqualsTestingUtility<ChessPiece> {
+    @Override
+    protected String getItemsPlural() {
+        return "pieces";
     }
 
-    @Test
-    @DisplayName("Equals Testing")
-    public void equalsTest() {
-        Assertions.assertEquals(original, equal, "equals returned false for equal pieces");
-        Assertions.assertNotEquals(original, different, "equals returned true for different pieces");
+    @Override
+    protected ChessPiece getOriginal() {
+        return new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
     }
 
-    @Test
-    @DisplayName("HashCode Testing")
-    public void hashTest() {
-        Assertions.assertEquals(original.hashCode(), equal.hashCode(),
-                "hashCode returned different values for equal pieces");
-        Assertions.assertNotEquals(original.hashCode(), different.hashCode(),
-                "hashCode returned the same value for different pieces");
+    @Override
+    protected ChessPiece getEqual() {
+        return new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
     }
 
-    @Test
-    @DisplayName("Equals & HashCode Testing")
-    public void hashSetTest() {
-        // FIXME: Add thorough testing for equality differences
+    @Override
+    protected Collection<ChessPiece> getAllDifferent() {
+        return List.of(
+                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN)
+        );
     }
+
 }
---
+++ shared/src/test/java/passoff/chess/EqualsTestingUtility.java
@@ -0,0 +1,78 @@
+package passoff.chess;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class EqualsTestingUtility<T> {
+    private String itemsPlural;
+    private T original;
+    private T equal;
+    private Collection<T> allDifferent;
+
+    protected abstract String getItemsPlural();
+    protected abstract T getOriginal();
+    protected abstract T getEqual();
+    protected abstract Collection<T> getAllDifferent();
+
+
+    @BeforeEach
+    public void setUp() {
+        itemsPlural = getItemsPlural();
+        original = getOriginal();
+        equal = getEqual();
+        allDifferent = getAllDifferent();
+    }
+
+    @Test
+    @DisplayName("Equals Testing")
+    public void equalsTest() {
+        Assertions.assertEquals(original, equal, "equals returned false for equal " + itemsPlural);
+        for (var different : allDifferent) {
+            Assertions.assertNotEquals(original, different, "equals returned true for different " + itemsPlural);
+        }
+    }
+
+    @Test
+    @DisplayName("HashCode Testing")
+    public void hashTest() {
+        Assertions.assertEquals(original.hashCode(), equal.hashCode(),
+                "hashCode returned different values for equal " + itemsPlural);
+        for (var different : allDifferent) {
+            Assertions.assertNotEquals(original.hashCode(), different.hashCode(),
+                    "hashCode returned the same value for different " + itemsPlural);
+        }
+    }
+
+    @Test
+    @DisplayName("Equals & HashCode Testing")
+    public void hashSetTest() {
+        Set<T> set = new HashSet<>();
+        set.add(original);
+
+        // Manually test insertion of original & equal items
+        Assertions.assertTrue(set.contains(original));
+        Assertions.assertTrue(set.contains(equal));
+        Assertions.assertEquals(1, set.size());
+        set.add(equal);
+        Assertions.assertEquals(1, set.size());
+
+        // Programmatically test insertion of all different items
+        int expectedSetSize = 1;
+        for (var different : allDifferent) {
+            Assertions.assertFalse(set.contains(different),
+                    "Different item should not be present in set before insertion");
+            set.add(different);
+            expectedSetSize++;
+            Assertions.assertEquals(expectedSetSize, set.size(),
+                    "New item was counted as different during insertion");
+        }
+
+    }
+
+}
