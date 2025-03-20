ef2eb15f9619c1aed49cdb82605d8461d07b167a
mewilker <mewilk@byu.edu>
2025-03-19 18:34:21 -0600

    edit DatabaseManager.createDatabase to use a try with resources on it's connection

--- starter-code/4-database/dataaccess/DatabaseManager.java
+++ starter-code/4-database/dataaccess/DatabaseManager.java
@@ -35,12 +35,10 @@ public class DatabaseManager {
      * Creates the database if it does not already exist.
      */
     static void createDatabase() throws DataAccessException {
-        try {
-            var statement = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
-            var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
-            try (var preparedStatement = conn.prepareStatement(statement)) {
+        var statement = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
+        try(var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
+            var preparedStatement = conn.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
-            }
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
         }
