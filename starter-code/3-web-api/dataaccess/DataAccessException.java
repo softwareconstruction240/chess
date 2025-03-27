05cd32ee5a441e13b5c237846f42db13107c6acc
James Finlinson <finljam@byu.edu>
2025-03-27 16:26:06 -0600

    Update DataAccessException.java Restore String only constructor
    This fulfills the request by @jerodw.
    
    Currently, this new constructor is unused. 
    @19mdavenport Will this cause any issues with the 
    style checker and unused code?

--- starter-code/3-web-api/dataaccess/DataAccessException.java
+++ starter-code/3-web-api/dataaccess/DataAccessException.java
@@ -4,6 +4,9 @@ package dataaccess;
  * Indicates there was an error connecting to the database
  */
 public class DataAccessException extends Exception{
+    public DataAccessException(String message) {
+        super(message);
+    }
     public DataAccessException(String message, Throwable ex) {
         super(message, ex);
     }
