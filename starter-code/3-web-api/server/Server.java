57d95c638e13a5abc2c3a6a34b58ec9f6db39143
Ken Rodham <ken.rodham@gmail.com>
2025-03-28 11:59:18 -0600

    Updated starter project to use Javalin instead of Spark. Updated library dependencies.

--- starter-code/3-web-api/server/Server.java
+++ starter-code/3-web-api/server/Server.java
@@ -1,25 +1,24 @@
 package server;
 
-import spark.*;
+import io.javalin.*;
 
 public class Server {
 
+    private final Javalin javalin;
+
+    public Server() {
+        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
+        // Register your endpoints and exception handlers here.
+
+    }
+
     public int run(int desiredPort) {
-        Spark.port(desiredPort);
-
-        Spark.staticFiles.location("web");
-
-        // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
-
-        Spark.awaitInitialization();
-        return Spark.port();
+        javalin.start(desiredPort);
+        return javalin.port();
     }
 
     public void stop() {
-        Spark.stop();
-        Spark.awaitStop();
+        javalin.stop();
     }
 }
