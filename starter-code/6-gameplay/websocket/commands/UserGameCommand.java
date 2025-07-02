41566df98e3673ac24297370e4a6fb37ac9b5c0f
lee <leesjensen@gmail.com>
2025-07-02 08:42:14 -0600

    enhance(starter code) with instanceof variable creation

--- starter-code/6-gameplay/websocket/commands/UserGameCommand.java
+++ starter-code/6-gameplay/websocket/commands/UserGameCommand.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 /**
  * Represents a command a user can send the server over a websocket
- *
+ * <p>
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */
@@ -46,10 +46,9 @@ public class UserGameCommand {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof UserGameCommand)) {
+        if (!(o instanceof UserGameCommand that)) {
             return false;
         }
-        UserGameCommand that = (UserGameCommand) o;
         return getCommandType() == that.getCommandType() &&
                 Objects.equals(getAuthToken(), that.getAuthToken()) &&
                 Objects.equals(getGameID(), that.getGameID());
--- starter-code/6-gameplay/websocket/messages/ServerMessage.java
+++ starter-code/6-gameplay/websocket/messages/ServerMessage.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 /**
  * Represents a Message the server can send through a WebSocket
- * 
+ * <p>
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */
@@ -30,10 +30,9 @@ public class ServerMessage {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof ServerMessage)) {
+        if (!(o instanceof ServerMessage that)) {
             return false;
         }
-        ServerMessage that = (ServerMessage) o;
         return getServerMessageType() == that.getServerMessageType();
     }
 
